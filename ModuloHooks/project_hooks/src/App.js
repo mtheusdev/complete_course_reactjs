a689bbe8e5c0750213d0398d2acf51352409cac2
mtheusdev <matheus.santos@edu.udesc.br>
2021-04-16 21:15:46 -0300

    finalizando useEffects

--- ModuloHooks/project_hooks/src/App.js
+++ ModuloHooks/project_hooks/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 function App() {
-
-  const [reverse, setReverse] = useState(false)
   const [counter, setCounter] = useState(0)
-  const reverseClass = reverse ? 'reverse' : ''
+  const [counter2, setCounter2] = useState(0)
+  //componentDidUpdate = executa toda vez que o componente atualizar e for criado
+  useEffect(() => {
+    console.log('update')
+  })
+  //componentDidMount = executa uma vez quando criar
+  useEffect(() => {
+    console.log('montando')
+    return () => {
+      // aqui é quando desmontar
+    }
+  }, [])
 
-  const handleClick = () =>  {
-    setReverse(!reverse)
-    setCounter(counter +1)
-  }
+  //componentDidMount = executa uma vez quando a dependencia mudar (counter)
+  useEffect(() => {
+    console.log('contador mudou ', counter)
+  }, [counter])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>Contador: {counter}</h1>
-        <button onClick={handleClick} type="button">Reverse</button>
-      </header>
+      <h1>Contador 1: {counter} | Contador 2: {counter2}</h1>
+      <button onClick={() => {setCounter(counter + 1)}}>Adicionar</button>
+      <button onClick={() => {setCounter2(counter2 + 1)}}>Adicionar 2</button>
     </div>
   );
 }
