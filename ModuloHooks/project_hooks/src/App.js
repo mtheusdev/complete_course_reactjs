0b23f128e09167e11339597cc65706bed5fa1e54
mtheusdev <matheus.santos@edu.udesc.br>
2021-04-16 22:01:09 -0300

    finalizando useCallback

--- ModuloHooks/project_hooks/src/App.js
+++ ModuloHooks/project_hooks/src/App.js
@@ -1,30 +1,24 @@
+import P from 'prop-types'
 import './App.css';
-import { useState, useEffect } from 'react'
+import React, { useState, useCallback} from 'react'
+
+const Button = React.memo(({incrementButton}) => {
+  return  <button onClick={() => incrementButton(10)}>Adicionar</button>
+})
+
+Button.prototypes = {
+  incrementButton: P.func
+}
+
 function App() {
   const [counter, setCounter] = useState(0)
-  const [counter2, setCounter2] = useState(0)
-  //componentDidUpdate = executa toda vez que o componente atualizar e for criado
-  useEffect(() => {
-    console.log('update')
-  })
-  //componentDidMount = executa uma vez quando criar
-  useEffect(() => {
-    console.log('montando')
-    return () => {
-      // aqui é quando desmontar
-    }
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num)
   }, [])
-
-  //componentDidMount = executa uma vez quando a dependencia mudar (counter)
-  useEffect(() => {
-    console.log('contador mudou ', counter)
-  }, [counter])
-
   return (
     <div className="App">
-      <h1>Contador 1: {counter} | Contador 2: {counter2}</h1>
-      <button onClick={() => {setCounter(counter + 1)}}>Adicionar</button>
-      <button onClick={() => {setCounter2(counter2 + 1)}}>Adicionar 2</button>
+      <h1>Contador 1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
